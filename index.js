4ec281f8c8bf65307ebb8ea7da66d46e8d9115ec
root <shajalahamedcse@gmail.com>
2024-06-27 09:40:06 +0000

    Added to github

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+res.status(200).send(`Hello, from Node App on PORT: ${port}!`);
+});
+
+app.listen(port, () => {
+console.log(`App running on http://localhost:${port}`);
+});
